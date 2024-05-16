43e924f7c1ab4ab92b35a08275e83916f191d55f
Fuad_Hashimov <fuadheshimzade@gmail.com>
2024-05-17 00:41:03 +0400

    Add login register

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
@@ -17,15 +20,55 @@ class LoginController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
-            'password' => 'required'
+            'password' => 'required|max:12|min:4'
         ]);
 
         if ($validator->passes()) {
-            
+            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+                return redirect()->route('account.home');
+            } else {
+                return redirect()->route('account.login')->with('error','Email or password incorrect');
+            }
         } else {
             return redirect()->route('account.login')
-            ->withInput()
-            ->withErrors($validator);
+                ->withInput()
+                ->withErrors($validator);
         }
+    }
+
+    public function register()
+    {
+        return view('register');
+    }
+
+    public function processRegister(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        if ($validator->passes()) {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->role = 'customer';
+            $user->save();
+
+            return redirect()->route('account.login')
+                ->with('success','Register success');
+        }
+        else {
+            return redirect()->route('account.register')
+                ->withInput()
+                ->withErrors($validator);
+        }
+    }
+
+    public function logOut(){
+        Auth::logout();
+        return redirect()->route('account.login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,24 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/login',[LoginController::class,'index'])->name('account.login');
+Route::group(['prefix' => 'account'], function () {
+    Route::group(['middleware' => 'guest'], function () {
+        Route::get('login', [LoginController::class, 'index'])->name('account.login');
+        Route::post('authenticate', [LoginController::class, 'authenticate'])->name('account.authenticate');
+        Route::get('register', [LoginController::class, 'register'])->name('account.register');
+        Route::post('process-register', [LoginController::class, 'processRegister'])->name('account.processRegister');
+    });
+
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('logOut', [LoginController::class, 'logOut'])->name('account.logOut');
+        Route::get('home', [HomeController::class, 'index'])->name('account.home');
+    });
+});
+ 
